374b4bdeb43ea913184d2da30181a7c34d803e3b
u1078811 <adamos.spanashis@iqvia.com>
2023-12-26 19:39:51 +0000

    Moves files to a flat hierarchy

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
 from setuptools import setup
 
 setup(
-    name="ordinalgbt",
-    version="0.01",
-    description="",
-    author="Adamos",
-    package_dir={"": "src"},
-    classifiers=[
-        "Programming Language :: Python :: 3.8",
-    ],
 )
